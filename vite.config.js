2d02b251f04b0c037c7a25d17c1ad4863a37ebf2
Jacob Stephen <Cashflow34steph@gmail.com>
2026-08-19 04:48:40 +0100

    Updated vite config js

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        portal: resolve(__dirname, 'portal.html'),
+        calendar: resolve(__dirname, 'calendar.html')
+      }
+    }
+  }
+});
